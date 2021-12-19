3d8690c33fdcbaf367cc30571ec4056cb6697303
AS <yvg68@yandex.ru>
2021-12-19 19:37:35 +0300

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val my: String = "I am going to learn Kotlin"
+
+    println(my)
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
